81bc8743c0dc94cd3c49f050aae460181a218492
salinna <45951344+salinna@users.noreply.github.com>
2024-06-11 15:14:18 +0100

    Update firebaseConfig.ts

--- app/config/firebaseConfig.ts
+++ app/config/firebaseConfig.ts
@@ -1,15 +1,18 @@
 import { initializeApp } from 'firebase/app'
 import { getAuth } from 'firebase/auth'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 // Firebase configuration
 const firebaseConfig = {
-  apiKey: 'AIzaSyChTaLcrzpNOfO9JLTBDu2DnA5BGsFzqvY',
-  authDomain: 'motics-legal-ai.firebaseapp.com',
-  projectId: 'motics-legal-ai',
-  storageBucket: 'motics-legal-ai.appspot.com',
-  messagingSenderId: '822715242904',
-  appId: '1:822715242904:web:19122f9c444aed55c0f3ed',
-  measurementId: 'G-305NWC1D6P'
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 }
 
 // Initialize Firebase
